f5e543fa52ea08f3ab187801af704799c19c8696
my5t3ry <sash@mischkonsum.org>
2020-05-15 15:13:26 +0200

    api module renamed

--- eterna-list-api/src/main/java/de/my5t3ry/eternal/encryption/KeyService.java
+++ eterna-list-api/src/main/java/de/my5t3ry/eternal/encryption/KeyService.java
@@ -82,7 +82,7 @@ public class KeyService {
                 | InvalidAlgorithmParameterException
                 | InvalidKeyException
                 | InvalidParameterSpecException e) {
-            throw new IllegalStateException("failed to encrypt private key with secret.", e);
+            throw new IllegalStateException(String.format("could not convert key pair ['%s'].", e.getMessage()), e);
         }
     }
 
